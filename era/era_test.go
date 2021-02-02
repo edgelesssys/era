606957b8cee0ae666c0b183e09f257f966e30fda
Nils Hanke <nils.hanke@rub.de>
2021-02-02 14:58:36 +0100

    Add unit test for parsing multiple certificates

--- era/era_test.go
+++ era/era_test.go
@@ -1,13 +1,18 @@
 package era
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
 	"crypto/sha256"
+	"crypto/x509"
 	"encoding/json"
 	"encoding/pem"
 	"errors"
+	"math/big"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/edgelesssys/ertgolib/ert"
 	"github.com/stretchr/testify/assert"
@@ -143,9 +148,84 @@ func TestGetCertificate(t *testing.T) {
 	assert.NotNil(err)
 }
 
+func TestGetMultipleCertificates(t *testing.T) {
+	config := []byte(`
+	{
+		"securityVersion": 2,
+		"productID": 3,
+		"signerID": "ABCD"
+	}
+	`)
+
+	assert := assert.New(t)
+	var quote []byte
+	var certs string
+
+	server, addr, expectedCerts := newServerMultipleCertificates(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal("/quote", r.RequestURI)
+		jsn, err := json.Marshal(certQuoteResp{certs, quote})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		w.Write(jsn)
+	}))
+	certs = expectedCerts[0] + expectedCerts[1]
+	block, _ := pem.Decode([]byte(expectedCerts[1])) // last one is supposed to be root CA, which we use for quoting
+	certRaw := block.Bytes
+	hash := sha256.Sum256([]byte(certRaw))
+	quote = hash[:]
+
+	defer server.Close()
+
+	// get certificates without quote validation
+	actualCerts, err := getCertificate(addr, nil, nil)
+	assert.Nil(err)
+	assert.EqualValues(expectedCerts[0], pem.EncodeToMemory(actualCerts[0]))
+	assert.EqualValues(expectedCerts[1], pem.EncodeToMemory(actualCerts[1]))
+
+	// get certificates with quote validation
+	actualCerts, err = getCertificate(addr, config,
+		func(reportBytes []byte) (ert.Report, error) {
+			assert.Equal(quote, reportBytes)
+			return ert.Report{
+				Data:            hash[:],
+				SecurityVersion: 2,
+				ProductID:       []byte{0x03, 0x00},
+				SignerID:        []byte{0xAB, 0xCD},
+			}, nil
+		})
+	assert.Nil(err)
+	assert.EqualValues(expectedCerts[0], pem.EncodeToMemory(actualCerts[0]))
+	assert.EqualValues(expectedCerts[1], pem.EncodeToMemory(actualCerts[1]))
+}
+
 func newServer(handler http.Handler) (server *httptest.Server, addr string, cert string) {
 	s := httptest.NewTLSServer(handler)
 	return s, s.Listener.Addr().String(), toPEM(s.Certificate().Raw)
+}
+
+func newServerMultipleCertificates(handler http.Handler) (server *httptest.Server, addr string, certs []string) {
+	// Create a second test certificate
+	key, err := rsa.GenerateKey(rand.Reader, 3096)
+	if err != nil {
+		panic(err)
+	}
+
+	template := x509.Certificate{
+		SerialNumber: big.NewInt(42),
+		IsCA:         false,
+		NotBefore:    time.Now(),
+		NotAfter:     time.Now().Add(time.Hour * 24 * 365),
+	}
+
+	testCertRaw, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
+	if err != nil {
+		panic(err)
+	}
+
+	s := httptest.NewTLSServer(handler)
+	expectedCerts := []string{toPEM(testCertRaw), toPEM(s.Certificate().Raw)}
+	return s, s.Listener.Addr().String(), expectedCerts
 }
 
 func toPEM(certificate []byte) string {
